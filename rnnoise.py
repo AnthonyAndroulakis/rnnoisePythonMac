294ffd2155dfa69d6c4382f62e10e60c095f1732
Anthony Androulakis <aandroulakis@zoho.com>
2020-01-28 21:39:53 -0500

    Add files via upload

---
+++ rnnoise.py
@@ -0,0 +1,22 @@
+#rnnoise.py, requirements: ffmpeg, sox, rnnoise, python
+import os, sys
+
+def run(filename):
+    os.system("mkdir -p inputraw")
+    os.system("mkdir -p outputraw")
+    os.system("mkdir -p rnnoiseoutput")
+
+    print("Pre-processing audio...")
+    os.system("ffmpeg -i "+filename+" -f s16le -ac 1 -ar 48000 ./inputraw/"+filename[:-4]+".raw") #convert to raw
+
+    print("Applying rnnoise algorithm to audio...")
+    os.system("./rnnoise/examples/rnnoise_demo ./inputraw/"+filename[:-4]+".raw ./outputraw/"+filename[:-4]+".raw")
+
+    print("Post-processing audio...")
+    os.system("sox -t raw -r 48000 -b 16 -e signed-integer -c 1 outputraw/"+filename[:-4]+".raw rnnoiseoutput/"+filename)
+    os.system("rm -rf inputraw")
+    os.system("rm -rf outputraw")
+
+    print("Audio-filtering completed!")
+
+run(sys.argv[1])
