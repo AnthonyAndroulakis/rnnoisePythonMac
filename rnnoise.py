8ea8ef4f3109390d04aaf5c678f3b555b55c5439
Anthony Androulakis <aandroulakis@zoho.com>
2020-01-28 23:56:26 -0500

    Update rnnoise.py

--- rnnoise.py
+++ rnnoise.py
@@ -6,13 +6,13 @@ def run(filename):
     os.system("mkdir -p outputraw")
     os.system("mkdir -p rnnoiseoutput")
 
-    print("Pre-processing audio...")
+    print("Pre-processing audio...") #wav to pcm raw
     os.system("ffmpeg -i "+filename+" -f s16le -ac 1 -ar 48000 ./inputraw/"+filename[:-4]+".raw") #convert to raw
 
-    print("Applying rnnoise algorithm to audio...")
+    print("Applying rnnoise algorithm to audio...") #rnnoise
     os.system("./rnnoise/examples/rnnoise_demo ./inputraw/"+filename[:-4]+".raw ./outputraw/"+filename[:-4]+".raw")
 
-    print("Post-processing audio...")
+    print("Post-processing audio...") #pcm raw to wav
     os.system("sox -t raw -r 48000 -b 16 -e signed-integer -c 1 outputraw/"+filename[:-4]+".raw rnnoiseoutput/"+filename)
     os.system("rm -rf inputraw")
     os.system("rm -rf outputraw")
