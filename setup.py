8636d2fec867ce132ecafe63760b491759b70ab6
Anthony Androulakis <aandroulakis@zoho.com>
2020-01-28 21:46:57 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("git clone https://github.com/AnthonyAndroulakis/rnnoisePythonMac.git")
+os.chdir("rnnoisePythonMac")
+os.system("git clone https://github.com/xiph/rnnoise.git")
+os.chdir("rnnoise")
+os.system("./autogen.sh")
+os.system("./configure")
+os.system("make")
+os.chdir("..")
+print("You can now run rnnoise.py using this format: python rnnoise.py input.wav")
